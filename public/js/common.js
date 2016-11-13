64d5c9ca6ac32eb0dc8abf57c32c04a0d6b1525c
Egor <egortictac@amil.ru>
2016-11-14 00:03:11 +0300

    new version

--- public/js/common.js
+++ public/js/common.js
@@ -1,182 +1,251 @@
-$(document).ready(function () {
-    $('#button12').attr('disabled', true);
-    $('#header1').hide();
-    $('#header2').hide();
-    $('.hint2').hide();
-    
-    
-    
-    $('input.chb').on('change', function () {
-      
-        $('input.chb').not(this).prop('checked', false);
-        
-        
-    });
-    
-//    function () {
-//        if ($("#Ship1").is(':checked') || $("#Ship2").is(':checked') || $("#Ship3").is(':checked') || $("#Ship4").is(':checked') ) {
-//            $('.button12').attr('disabled', false);
-//        } else {
-//            $('.button12').attr('disabled', true);
-//        }
-//    };
-
-    $('input.chb').click(function () {
-    
-        if ($('input.chb').is(":checked")) {
-            window.checkedValue = $(this).val();
-            console.log(window.checkedValue);
-            $('#button12').attr('disabled', false)
-            $('.hint1').fadeOut();
-            $('.hint2').fadeIn();
-            
-        }
-        else
-            {
-                $('#button12').attr('disabled', true);
-            }
-    })
-
+$(window).on('load', function () {
+	$(".loader").fadeOut("slow");
 });
 
+$(document).ready(function () {
+	$('#header1').fadeTo(0, 0);
+	$('#header2').fadeTo(0, 0);
+	$('#search1').fadeTo(0, 0);
+	$('#search2').fadeTo(0, 0);
+	$('#button12').attr('disabled', true);
+	$('.hint1').fadeIn();
+	$('.hint2').fadeTo(0, 0);
 
+
+	$('input.chb').on('change', function () {
+		$('input.chb').not(this).prop('checked', false);
+	});
+
+	$('input.chb').click(function () {
+		if ($('input.chb').is(":checked")) {
+			window.checkedValue = $(this).val();
+			console.log(window.checkedValue);
+			$('#button12').attr('disabled', false)
+			$('.hint1').fadeTo('medium', 0);
+			$('.hint2').animate({
+				opacity: 1
+			});
+
+		} else {
+			$('#button12').attr('disabled', true);
+		}
+	})
+});
 
 $(document).ready(function () {
-    $("#button12").click(function () {
-        console.clear();
-        $('.hint2').fadeOut();
-        event.preventDefault();
-        console.log(window.checkedValue);
-        //url on server http://spun.fkpkzs.ru/Level/Gorny
-        $.ajax({
-            data: {
-                name: "some_name",
-                value: window.checkedValue
-            },
-            type: "POST",
-            url: "https://floating-shore-25832.herokuapp.com/scrape",
+	// Note, that I'm adding the argument event to .click function. It made for Mozila Browser.
+	$("#button12").click(function (event) {
+		console.clear();
+		$('.hint2').fadeTo('medium', 0);
+		event.preventDefault();
+		console.log(window.checkedValue);
+		//url on server http://spun.fkpkzs.ru/Level/Gorny
+		$.ajax({
+			data: {
+				name: "some_name",
+				value: window.checkedValue
+			},
+			type: "POST",
+			url: "https://floating-shore-25832.herokuapp.com/scrape",
 
-            //if post request is successful -> do function with logic
-            success: function (data) {
-                
-                $('#header1').hide();
-                $('#header2').hide();
+			//if post request is successful -> do function with logic
+			success: function (data) {
 
-                // checking the content of server response
-                console.log("success " + data.time + " " + data.waterlevel + " " + data.shipValue.value);
+				$('#header1').fadeTo('fast', 0);
+				$('#header2').fadeTo('fast', 0);
 
-                // Appending the conten on HTML in div with id="resposnse21" 
-//                $("#response21")
-//                    .empty()
-//                    .append("Time: " + data.time + "<br>").hide().fadeIn()
-//                    .append("Waterlevel: " + data.waterlevel + "<br>").hide().fadeIn()
-//                    .append("Ship type: " + data.shipValue.value + "<br>").hide().fadeIn();
-                window.waterlevel_time = data.time;
-                window.waterlevel = data.waterlevel;
-                window.shipValue = data.shipValue.value;
+				// checking the content of server response
+				console.log("success " + data.time + " " + data.waterlevel + " " + data.shipValue.value);
 
-                //logic of sorting array of bridges
-                
-                //sample json array of bridges
-                var obj = [
-                    {
-                        name: "Great_asdjkgaflkhgasljvhk",
-                        height: 340
+				// Appending the content on HTML in div with id="resposnse21" 
+				//                $("#response21")
+				//                    .empty()
+				//                    .append("Time: " + data.time + "<br>").hide().fadeIn()
+				//                    .append("Waterlevel: " + data.waterlevel + "<br>").hide().fadeIn()
+				//                    .append("Ship type: " + data.shipValue.value + "<br>").hide().fadeIn();
+				window.waterlevel_time = data.time;
+				window.waterlevel = data.waterlevel;
+				window.shipValue = data.shipValue.value;
+
+				//logic of sorting bridges array 
+
+				//sample json array of bridges
+				var obj = [
+					{
+						name: "Great_asdjkgaflkhgasljvhk",
+						height: 340
                         },
-                    {
-                        name: "Shit",
-                        height: 270
+					{
+						name: "Shit",
+						height: 270
                         },
-                    {
-                        name: "Hello",
-                        height: 400
+					{
+						name: "Hello",
+						height: 400
                         },
-                    {
-                        name: "b1",
-                        height: 210
+					{
+						name: "b1",
+						height: 210
                         },
-                    {
-                        name: "b2",
-                        height: 300
+					{
+						name: "b2",
+						height: 300
                         }
                     ]
 
-                //initilizing new array for suitable
-                var obj_g = [];
+				//initilizing new array for suitable bridges
+				var obj_g = [];
 
-                console.log(window.shipValue);
-                console.log(window.waterlevel);
+				console.log(window.shipValue);
+				console.log(window.waterlevel);
 
-                //parsing string data respond from server to get integers
-                var shipandwater = parseInt(window.shipValue) + parseInt(window.waterlevel);
-                console.log(shipandwater);
+				//parsing string data respond from server to get integers
+				var shipandwater = parseInt(window.shipValue) + parseInt(window.waterlevel);
+				console.log(shipandwater);
 
-                //sorting
-                for (var i = 0; i < obj.length; i++) {
-                    console.log("inside for every obj[i].height: " + obj[i].height)
-                    if (obj[i].height > shipandwater)
+				//sorting
+				for (var i = 0; i < obj.length; i++) {
+					console.log("inside for every obj[i].height: " + obj[i].height)
+					if (obj[i].height > shipandwater)
 
-                    //new array of bridges that ship can pass under
-                    {
-                        console.log("Inside if op " + obj[i])
-                        obj_g.push(obj[i]);
-                    }
+					//new array of bridges that ship can pass under
+					{
+						console.log("Inside if op " + obj[i])
+						obj_g.push(obj[i]);
+					}
 
-                }
-                console.log("obj_g length:" + obj_g.length)
-
-                console.log("obj_g: ")
-                for (var i = 0; i < obj_g.length; i++) {
-                    console.log(" " + obj_g[i].name + " " + obj_g[i].height + "\n")
-
-                }
-
-
-                // building 2 tables with all bridges and valid  bridges
-                // http://jsfiddle.net/URU5G/1/ fiddle with example of table from json
-                
-                
-               
-                
-                $("#table1").empty();
-
-                $("#table2").empty();
-
-                var tbl1 = $("<table>").attr("id", "all_bridges");
-                $("#table1").append(tbl1).fadeIn(600);
-                for (var i = 0; i < obj.length; i++) {
-                    var tr = "<tr>";
-                    var td1 = "<td>" + obj[i].name + "</td>";
-                    var td2 = "<td>" + obj[i].height + "</td></tr>";
-
-                    $("#all_bridges").append(tr + td1 + td2).fadeIn();
-                     $('#header1').fadeIn(800);
-                };
-               
-                 
-
-                        
-                var tbl2 = $("<table>").attr("id", "vaildBridges");
-                $("#table2").append(tbl2);
-                for (var i = 0; i < obj_g.length; i++) {
-                    var tr = "<tr>";
-                    var td1 = "<td>" + obj_g[i].name + "</td>";
-                    var td2 = "<td>" + obj_g[i].height + "</td></tr>";
+				}
+				//                console.log("obj_g length:" + obj_g.length)
+				//
+				//                console.log("obj_g: ")
+				//                for (var i = 0; i < obj_g.length; i++) {
+				//                    console.log(" " + obj_g[i].name + " " + obj_g[i].height + "\n")
+				//
+				//                }
 
 
-                    $("#vaildBridges").append(tr + td1 + td2).fadeIn();
-                    $('#header2').fadeIn(800);
-                };
-                  
+				// building 2 tables with all bridges and valid  bridges
+				// http://jsfiddle.net/URU5G/1/ fiddle with example of table from json
+				$("#table1").empty();
 
-            },
-            error: function (req, status, err) {
-                console.log('Something went wrong', status, err);
-                console.log(err)
+				$("#table2").empty();
+				var fadeInduration = 300;
 
-            }
+				$('#header1').animate({
+					duration: 300,
+					opacity: 1
+				});
+
+				var tbl1 = $("<table><tr class='t1'><th>Название</th><th>Высота(см)</th></tr>").attr("id", "all_bridges");
+				//$("#table1").append(tbl1);
+				$(tbl1).hide().appendTo("#table1").fadeIn(300);
+				for (var i = 0; i < obj.length; i++) {
+					var tr = "<tr class='t1'>";
+					var td1 = "<td>" + obj[i].name + "</td>";
+					var td2 = "<td>" + obj[i].height + "</td></tr>";
+
+					$(tr + td1 + td2).hide().appendTo("#all_bridges").fadeIn(300);
+					//$("#all_bridges").append(tr + td1 + td2).fadeIn();
+				};
+
+				$('#header2').animate({
+					opacity: 1,
+					duration: 300
+				});
+				//<th>Bridge Name</th><th>Bridge Height</th>
+				var tbl2 = $("<table><tr class='t2'><th>Название</th><th>Высота(см)</th></tr>").attr("id", "vaildBridges");
+				$(tbl2).hide().appendTo("#table2").fadeIn(300);
+				for (var i = 0; i < obj_g.length; i++) {
+					var tr = "<tr class='t2'>";
+					var td1 = "<td>" + obj_g[i].name + "</td>";
+					var td2 = "<td>" + obj_g[i].height + "</td></tr>";
+
+					$(tr + td1 + td2).hide().appendTo("#vaildBridges").fadeIn(300);
+				};
+
+				//search in tables
+				
+				
+				//second search
+          
+          $('#search1').animate({
+					duration: 300,
+					opacity: 1
+				});
+          $('#search2').animate({
+					duration: 300,
+					opacity: 1
+				});
+				
+				$("#search1").keyup(function(){
+        _this = this;
+        // Show only matching TR, hide rest of them
+        $.each($("#all_bridges .t1"), function() {
+            if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
+               $(this).hide();
+            else
+               $(this).show();                
         });
-    });
+    }); 
+				$("#search2").keyup(function(){
+        _this = this;
+        // Show only matching TR, hide rest of them
+        $.each($("#vaildBridges .t2"), function() {
+            if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
+               $(this).hide();
+            else
+               $(this).show();                
+        });
+    }); 
+				
+				
+//				var $rows = $('#all_bridges .t2');
+//				$('#search2').keyup(function () {
+//					var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+//
+//					$rows.show().filter(function () {
+//						var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+//						return !~text.indexOf(val);
+//					}).hide();
+//				});
+//
+//				//first search
+//				var $rows = $('#vaildBridges .t1');
+//				$('#search1').keyup(function () {
+//					var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+//
+//					$rows.show().filter(function () {
+//						var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+//						return !~text.indexOf(val);
+//					}).hide();
+//				});
+
+
+
+				
+				//          
+				//          var $rows = $('#table2');
+				//				$('#search').keyup(function () {
+				//					var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+				//
+				//					$rows.fadeIn(200).filter(function () {
+				//						var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+				//						return !~text.indexOf(val);
+				//					}).fadeOut(200);
+				////          animate({
+				////					opacity: 0,
+				////					duration: 200
+				////				});
+				//				});
+
+			},
+			error: function (req, status, err) {
+				console.log('Something went wrong', status, err);
+				console.log(err)
+
+			}
+		});
+	});
 });
 
 
